66604623a733e233cb4bd551f4d59193158ad642
Christopher Trimboli <cjft@trimboli.io>
2025-04-26 09:27:38 -0600

    My agents page

--- app/agents/page.tsx
+++ app/agents/page.tsx
@@ -1,3 +1,30 @@
+import Activities from "@/components/dashboard/Activities";
+import MyAgents from "@/components/agents/MyAgents";
+import Streets from "@/components/dashboard/Streets";
+import TripleGrid from "@/components/TripleGrid";
+import Addons from "@/components/dashboard/Addons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Agent Street | My Agents",
+  description: "Your agents, your way.",
+};
+
 export default function Agents() {
-  return <div></div>;
+  return (
+    <TripleGrid
+      firstColumn={
+        <div className="flex flex-col gap-4">
+          <MyAgents />
+        </div>
+      }
+      secondColumn={<Activities />}
+      thirdColumn={
+        <div className="flex flex-col gap-4">
+          <Streets />
+          <Addons />
+        </div>
+      }
+    />
+  );
 }
