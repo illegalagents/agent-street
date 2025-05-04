af46bde6c60e61027f2bb188b74e828115256cb4
Christopher Trimboli <cjft@trimboli.io>
2025-05-03 19:52:00 -0600

    fix animation jank

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import FadeInWrapper from "@/components/FadeInWrapper";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
@@ -18,7 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="relative h-screen antialiased overflow-hidden">
         <Navbar />
-        <FadeInWrapper>{children}</FadeInWrapper>
+        <main className="absolute top-16 bottom-0 left-0 right-0 overflow-y-auto">
+          {children}
+        </main>
       </body>
       <Analytics />
     </html>
